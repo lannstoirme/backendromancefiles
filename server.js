537b807d93b7e4f01ff60435ec389b5dbba22f75
Lann Stoirme <almonddigitaltechnologies@gmail.com>
2019-03-06 15:10:40 +1000

    update
    updated errors

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ http.createServer(function(request, response) {
     response.writeHead(200, {
         'Content-Type': 'text/json',
 		'Access-Control-Allow-Origin': '*',
-		'X-Powered-By':'nodejs'
+		'X-Powered-By':'nodejs',
     });
 
 
-    fs.readFile('/romance.json', function(err, content){
+    fs.readFile('romance.json', function(err, content){
         response.write(content);
         response.end();
     });
